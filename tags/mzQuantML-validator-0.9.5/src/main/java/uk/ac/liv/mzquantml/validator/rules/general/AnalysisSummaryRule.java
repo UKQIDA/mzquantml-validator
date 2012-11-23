e2fc332758a4098dced7e08249dda903a564ff70
ukqida@gmail.com <ukqida@gmail.com@90509d9d-dc48-32c1-d1b8-492df67e8a52>
2012-11-23 21:39:46 +0000

    [maven-release-plugin]  copy for tag mzQuantML-validator-0.9.5

---
+++ tags/mzQuantML-validator-0.9.5/src/main/java/uk/ac/liv/mzquantml/validator/rules/general/AnalysisSummaryRule.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.liv.mzquantml.validator.rules.general;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import uk.ac.liv.jmzqml.model.mzqml.CvParam;
+import uk.ac.liv.jmzqml.model.mzqml.ParamList;
+import uk.ac.liv.mzquantml.validator.utils.AnalysisSummaryElement;
+import uk.ac.liv.mzquantml.validator.utils.AnalysisType;
+import uk.ac.liv.mzquantml.validator.utils.AnalysisType.AnalTp;
+import uk.ac.liv.mzquantml.validator.utils.Message;
+
+/**
+ *
+ * @author Da Qi @institute University of Liverpool @time 13-Nov-2012 15:07:07
+ */
+public class AnalysisSummaryRule {
+
+    private static final Logger logger = Logger.getLogger(AssayLabelRule.class);
+    private ParamList anlaysisSummary;
+    private AnalysisType at;
+    private HashMap<String, AnalysisSummaryElement> cvTermMap;
+    List<Message> msgs = new ArrayList<Message>();
+
+    /*
+     * constructor
+     */
+    public AnalysisSummaryRule(ParamList analysisSummary) {
+        this.anlaysisSummary = analysisSummary;
+        this.at = new AnalysisType(analysisSummary);
+    }
+    /*
+     * public methods
+     */
+
+    public boolean check() {
+
+        boolean ret = true;
+        /**
+         * initialise @cvTermMap It records which valid cv terms are in
+         * AnalsisSummary by mapping accession to AnalysisSummaryElement
+         */
+        cvTermMap = new HashMap<String, AnalysisSummaryElement>();
+        // gather the cvTermMap
+        for (CvParam cv : this.anlaysisSummary.getCvParam()) {
+            String accession = cv.getAccession();
+            for (AnalysisSummaryElement ase : AnalysisSummaryElement.values()) {
+                if (ase.getAccession() != null && ase.getAccession().equals(accession)) {
+                    AnalysisSummaryElement a = cvTermMap.get(accession);
+                    if (a == null) {
+                        cvTermMap.put(accession, ase);
+                    } else {
+                        msgs.add(new Message("Duplicate cv term with accession: " + a.getAccession(), Level.ERROR));
+                    }
+                }
+            }
+        }
+
+        if (missCvTerm(cvTermMap, this.at)) {
+            ret = false;
+        }
+
+        return ret;
+    }
+
+    public List<Message> getMsgs() {
+        return msgs;
+    }
+
+    /*
+     * private methods
+     */
+    private boolean missCvTerm(HashMap<String, AnalysisSummaryElement> cvTermMap,
+            AnalysisType at) {
+
+        boolean ret = false;
+
+        for (AnalysisSummaryElement ase : AnalysisSummaryElement.valuesByType(at)) {
+            String accession = ase.getAccession();
+            if (cvTermMap.get(accession) == null) {
+                msgs.add(new Message("Missing cv term: " + ase.getAccession() + "(" + ase.getName() + ")" + " for " + at.getAnalysisType().getName(), Level.ERROR));
+                ret = true;
+            }
+        }
+
+        return ret;
+    }
+}
---
+++ tags/mzQuantML-validator-0.9.5/src/main/java/uk/ac/liv/mzquantml/validator/utils/AnalysisType.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.liv.mzquantml.validator.utils;
+
+import java.util.List;
+import uk.ac.liv.jmzqml.model.mzqml.AbstractParam;
+import uk.ac.liv.jmzqml.model.mzqml.CvParam;
+import uk.ac.liv.jmzqml.model.mzqml.ParamList;
+
+/**
+ *
+ * @author Da Qi
+ * @institute University of Liverpool
+ * @time May 3, 2012 2:48:50 PM
+ */
+public class AnalysisType {
+
+    public static enum AnalTp {
+
+        LabelFree("LC-MS label-free quantitation analysis", "MS:1001834"),
+        SpectralCounting("spectral counting quantitation analysis", "MS:1001836"),
+        MS1LabelBased("MS1 label-based analysis", "MS:1002018"),
+        MS2TagBased("MS2 tag-based analysis", "MS:1002023"),
+        InvalidAnalysisType("invalid name", "invalid accession");
+        private final String name;
+        private final String accession;
+
+        AnalTp(String name, String accession) {
+            this.name = name;
+            this.accession = accession;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAccession() {
+            return accession;
+        }
+
+        @Override
+        public String toString() {
+            return (this.getAccession() + ": " + this.getName());
+        }
+    };
+    AnalTp at;
+
+    public AnalysisType(ParamList analysisSummary) {
+        /*
+         * This can not deal with the situation when two valid cv terms are
+         * present.
+         * In reality, one experiment can only be from one techniqe.
+         * The type of techinque for mzq file will be decided by the first valid
+         * cv term.
+         */
+        List<AbstractParam> paramGroups = analysisSummary.getParamGroup();
+        for (AbstractParam param : paramGroups) {
+            //Technique tech = new Technique(param.getName());
+            if (param instanceof CvParam) {
+                CvParam cvParam = (CvParam) param;
+
+                if (cvParam.getAccession().equals(AnalTp.LabelFree.getAccession())) {
+                    at = AnalTp.LabelFree;
+                } else if (cvParam.getAccession().equals(AnalTp.SpectralCounting.getAccession())) {
+                    at = AnalTp.SpectralCounting;
+                } else if (cvParam.getAccession().equals(AnalTp.MS1LabelBased.getAccession())) {
+                    at = AnalTp.MS1LabelBased;
+                } else if (cvParam.getAccession().equals(AnalTp.MS2TagBased.getAccession())) {
+                    at = AnalTp.MS2TagBased;
+                } else {
+                    at = AnalTp.InvalidAnalysisType;
+                }
+                if (at != null) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public AnalysisType() {
+        at = null;
+    }
+
+    public AnalTp getAnalysisType() {
+        return this.at;
+    }
+}
