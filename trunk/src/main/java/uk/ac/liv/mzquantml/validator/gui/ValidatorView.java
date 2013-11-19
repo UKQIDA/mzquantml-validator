82706d3c326fb7feb890b939a486495d62abccc3
ukqida@gmail.com <ukqida@gmail.com@90509d9d-dc48-32c1-d1b8-492df67e8a52>
2013-11-19 14:22:06 +0000

    updated GUI, validation work flow and exception handling

--- trunk/src/main/java/uk/ac/liv/mzquantml/validator/gui/ValidatorView.java
+++ trunk/src/main/java/uk/ac/liv/mzquantml/validator/gui/ValidatorView.java
@@ -11,17 +11,27 @@
 package uk.ac.liv.mzquantml.validator.gui;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Collection;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import org.apache.log4j.Level;
+import psidev.psi.tools.ontology_manager.impl.local.OntologyLoaderException;
+import psidev.psi.tools.validator.ValidatorException;
+import psidev.psi.tools.validator.ValidatorMessage;
+import uk.ac.liv.mzquantml.validator.MzQuantMLSchemaValidator;
 import uk.ac.liv.mzquantml.validator.MzQuantMLValidator;
+import uk.ac.liv.mzquantml.validator.cvmapping.MzQuantMLCvValidator;
+import uk.ac.liv.mzquantml.validator.utils.Gzipper;
 import uk.ac.liv.mzquantml.validator.utils.Message;
 
 /**
@@ -31,6 +41,7 @@ import uk.ac.liv.mzquantml.validator.utils.Message;
 public class ValidatorView extends javax.swing.JFrame {
 
     private static File currentDirectory;
+    private static File mzqFile;
 
     /**
      * Creates new form ValidatorView
@@ -84,7 +95,7 @@ public class ValidatorView extends javax.swing.JFrame {
         jbValidate = new javax.swing.JButton();
         jtfSchema = new javax.swing.JTextField();
         jbSchema = new javax.swing.JButton();
-        jCheckBox1 = new javax.swing.JCheckBox();
+        schemaValidationCheckbox = new javax.swing.JCheckBox();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtaValidationResults = new javax.swing.JTextArea();
@@ -110,6 +121,7 @@ public class ValidatorView extends javax.swing.JFrame {
             }
         });
 
+        jtfSchema.setText("Use default schema mzQuantML_1_0_0.xsd");
         jtfSchema.setEnabled(false);
 
         jbSchema.setText("Schema");
@@ -120,7 +132,13 @@ public class ValidatorView extends javax.swing.JFrame {
             }
         });
 
-        jCheckBox1.setText("Schema validation");
+        schemaValidationCheckbox.setText("Schema validation (Warning: performing schema validation on large files could take hours!)");
+        schemaValidationCheckbox.setToolTipText("It is recommended to use other XML software for schema validation.");
+        schemaValidationCheckbox.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                schemaValidationCheckboxStateChanged(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -130,7 +148,7 @@ public class ValidatorView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jCheckBox1)
+                        .addComponent(schemaValidationCheckbox)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jbValidate))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
@@ -157,7 +175,7 @@ public class ValidatorView extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbValidate)
-                    .addComponent(jCheckBox1))
+                    .addComponent(schemaValidationCheckbox))
                 .addContainerGap())
         );
 
@@ -176,7 +194,7 @@ public class ValidatorView extends javax.swing.JFrame {
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -200,22 +218,68 @@ public class ValidatorView extends javax.swing.JFrame {
 
     private void jbValidateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbValidateActionPerformed
 
-        try {
-            //if (!this.jtfFileName.getText().isEmpty() && !this.jtfSchema.getText().isEmpty()) {
-            this.jtaValidationResults.setText("INFO: Starting validation process......\nINFO: Loading MzQuantML file......\n");
-            this.update(this.getGraphics());
-            String fileName = this.jtfFileName.getText();
-            MzQuantMLValidator mzqValidator = new MzQuantMLValidator(fileName, false, this.jtfSchema.getText());
-            List<Message> results = mzqValidator.validate(fileName, false, this.jtfSchema.getText());
-            this.jtaValidationResults.setLineWrap(true);
-            String results_mod = results.toString();
-            this.jtaValidationResults.append(results_mod.substring(2, results_mod.length() - 1));
-            //} else {
-            //    JOptionPane.showMessageDialog(this.jPanel2, "Missing mzq file or schema file!");
-            //}
+        long startTime = System.currentTimeMillis();
+        // Set the view to the the starting point every time click the validate button
+        this.jtaValidationResults.setText("");
+        this.jScrollPane1.getViewport().setViewPosition(new Point(0, 0));
+        this.update(this.getGraphics());
+
+        // Start validate.
+
+        String mzqFn = this.jtfFileName.getText();
+
+        if (mzqFn.isEmpty()) {
+            JOptionPane.showMessageDialog(this.jPanel2, "Please input an mzQuantML file!");
         }
-        catch (FileNotFoundException ex) {
-            Logger.getLogger(ValidatorView.class.getName()).log(Level.SEVERE, null, ex);
+        else {
+            this.jtaValidationResults.setLineWrap(true);
+            this.jtaValidationResults.setText("INFO: Starting validation process......\nINFO: Loading MzQuantML file......\n\n");
+            this.update(this.getGraphics());
+            if (mzqFn.endsWith(".mzq")) {
+                mzqFile = new File(mzqFn);
+            }
+            else if (mzqFn.endsWith(".gz")) {
+                this.jtaValidationResults.append("Uncompressing the gz file ... \n\n");
+                this.update(this.getGraphics());
+                mzqFile = getUnZipFile(mzqFn);
+            }
+//            try {
+            // if the schema validation checkbox is selected, do schema validation first
+            if (this.schemaValidationCheckbox.isSelected()) {
+                doSchemaValidation();
+                this.update(this.getGraphics());
+            }
+
+            // semantic validation
+            doSemantciValidation();
+
+            // cv mapping rule validation
+            try {
+                doCvMappingValidation();
+            }
+            catch (FileNotFoundException ex) {
+                this.jtaValidationResults.append("Can't find mzQuantML-mapping_1.0.0.xml or ontologies.xml! \n\n");
+                this.update(this.getGraphics());
+            }
+
+
+            /*
+             * final output
+             */
+            this.jtaValidationResults.append("\n\nMzQuantML validation process is finished.\n");
+            this.update(this.getGraphics());
+            long stopTime = System.currentTimeMillis();
+            System.out.println("Running time (ms): " + String.valueOf(stopTime - startTime));
+
+        }
+//            catch (Exception e) {
+//                this.jtaValidationResults.append(e.getMessage());
+//                this.update(this.getGraphics());
+//            }
+//        }
+        // clean the unzipped file from temp
+        if (mzqFn.endsWith(".gz")) {
+            Gzipper.deleteFile(mzqFile);
         }
     }//GEN-LAST:event_jbValidateActionPerformed
 
@@ -226,7 +290,7 @@ public class ValidatorView extends javax.swing.JFrame {
         fileChooser.setCurrentDirectory(currentDirectory);
 
         //... Applying file extension filters ...//
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("MzQuantML (*.mzq)", "mzq");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Uncompressed or Gzip Compressed MzQuantML (*.mzq; *.gz)", "mzq", "gz");
         fileChooser.setFileFilter(filter);
 
         int returnVal = fileChooser.showOpenDialog(this);
@@ -254,6 +318,12 @@ public class ValidatorView extends javax.swing.JFrame {
             currentDirectory = file.getParentFile();
         }
     }//GEN-LAST:event_jbSchemaActionPerformed
+
+    private void schemaValidationCheckboxStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_schemaValidationCheckboxStateChanged
+
+        this.jbSchema.setEnabled(schemaValidationCheckbox.isSelected());
+        this.jtfSchema.setEnabled(schemaValidationCheckbox.isSelected());
+    }//GEN-LAST:event_schemaValidationCheckboxStateChanged
 
     /**
      * @param args the command line arguments
@@ -302,7 +372,6 @@ public class ValidatorView extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
@@ -312,5 +381,158 @@ public class ValidatorView extends javax.swing.JFrame {
     private javax.swing.JTextArea jtaValidationResults;
     private javax.swing.JTextField jtfFileName;
     private javax.swing.JTextField jtfSchema;
+    private javax.swing.JCheckBox schemaValidationCheckbox;
     // End of variables declaration//GEN-END:variables
+
+    private void doSchemaValidation() {
+        // check if the schema file exists
+        if (!this.jtfSchema.getText().isEmpty() && (new File(this.jtfSchema.getText())).isFile()) {
+            File schemaFile = new File(this.jtfSchema.getText());
+            MzQuantMLSchemaValidator msv = new MzQuantMLSchemaValidator(mzqFile, schemaFile);
+            List<Message> svRes = msv.getValidateResult();
+
+            if (!svRes.isEmpty()) {
+                this.jtaValidationResults.append("\nSchema validation message(s): \n\n");
+                this.update(this.getGraphics());
+            }
+            else {
+                this.jtaValidationResults.append("\nDocument is schema valid.\n");
+                this.update(this.getGraphics());
+            }
+
+            for (Message msg : svRes) {
+                this.jtaValidationResults.append(msg.getMessage());
+                this.update(this.getGraphics());
+            }
+        }
+        else {
+            if (!new File(this.jtfSchema.getText()).isFile()) {
+                this.jtaValidationResults.append("Use default schema file mzQuantML_1_0_0.xsd.\n");
+                this.update(this.getGraphics());
+            }
+            try {
+                File schemaFile = getSchemaFile();
+
+                this.jtaValidationResults.append("\nStart schema validation (Warning: this could take hours for large file)...... \n");
+                this.update(this.getGraphics());
+
+                MzQuantMLSchemaValidator msv = new MzQuantMLSchemaValidator(mzqFile, schemaFile);
+                List<Message> svRes = msv.getValidateResult();
+
+                if (!svRes.isEmpty()) {
+                    this.jtaValidationResults.append("\nSchema validation message(s): \n\n");
+                    this.update(this.getGraphics());
+                }
+                else {
+                    this.jtaValidationResults.append("\nDocument is schema valid.\n");
+                    this.update(this.getGraphics());
+                }
+
+                for (Message msg : svRes) {
+                    this.jtaValidationResults.append(msg.getMessage());
+                    this.update(this.getGraphics());
+                }
+            }
+            catch (FileNotFoundException ex) {
+                this.jtaValidationResults.append("Can't find schema file mzQuantML_1_0_0.xsd! " + ex.getMessage() + "\n");
+                this.update(this.getGraphics());
+            }
+        }
+    }
+
+    private void doSemantciValidation() {
+        try {
+            MzQuantMLValidator mzqValidator = new MzQuantMLValidator(mzqFile);
+            List<Message> results = mzqValidator.validate();
+            if (!results.isEmpty()) {
+                this.jtaValidationResults.append("\nSemantic validation message(s): \n\n");
+                this.update(this.getGraphics());
+            }
+            for (Message m : results) {
+                this.jtaValidationResults.append(m.getLevel().toString() + ": " + m.getMessage());
+                this.update(this.getGraphics());
+            }
+        }
+        catch (FileNotFoundException ex) {
+            this.jtaValidationResults.append(ex.getMessage());
+            this.update(this.getGraphics());
+        }
+        catch (Exception e) {
+//            this.jtaValidationResults.append(e.getMessage());
+//            this.update(this.getGraphics());
+        }
+    }
+
+    private void doCvMappingValidation()
+            throws FileNotFoundException {
+        InputStream ontology = getOntologiesFileInputStream();
+        InputStream mappingRule = getGeneralMappingRuleInputStream();
+        MzQuantMLCvValidator cvValidator;
+        try {
+            cvValidator = new MzQuantMLCvValidator(ontology, mappingRule);
+            final Collection<ValidatorMessage> validationResult = cvValidator.startValidation(mzqFile.getAbsolutePath());
+
+            if (!validationResult.isEmpty()) {
+                this.jtaValidationResults.append("\nCv mapping validation message(s): \n");
+                this.update(this.getGraphics());
+            }
+            for (ValidatorMessage vMsg : validationResult) {
+                // convert MessageLevel to log4j Level
+                Level lev = Level.toLevel(vMsg.getLevel().toString());
+                if (vMsg.getRule() != null) {
+                    this.jtaValidationResults.append("\n" + lev + ": " + vMsg.getRule().toString() + " -- " + vMsg.getMessage() + "\n");
+                }
+                else {
+                    this.jtaValidationResults.append("\n" + lev + ": " + vMsg.getMessage() + "\n");
+                }
+                this.update(this.getGraphics());
+            }
+        }
+        catch (ValidatorException ve) {
+            this.jtaValidationResults.append("\nValidationException in cvValidator: " + ve.getMessage());
+            this.update(this.getGraphics());
+        }
+        catch (OntologyLoaderException ole) {
+            this.jtaValidationResults.append("\nOntologyLoaderException in cvValidator; " + ole.getMessage());
+            this.update(this.getGraphics());
+        }
+    }
+
+    private File getSchemaFile()
+            throws FileNotFoundException {
+        File file = new File(getClass().getClassLoader().getResource("mzQuantML_1_0_0.xsd").getFile());
+        return file;
+    }
+
+    private File getUnZipFile(String mzqFn) {
+        File f = Gzipper.extractFile(new File(mzqFn));
+        return f;
+    }
+
+    /*
+     * protected methods
+     */
+    protected InputStream getOntologiesFileInputStream()
+            throws FileNotFoundException {
+
+        File file = new File(getClass().getClassLoader().getResource("ontologies.xml").getFile());
+        if (!file.exists()) {
+            ClassLoader cl = this.getClass().getClassLoader();
+            return cl.getResourceAsStream("ontologies.xml");
+        }
+        return new FileInputStream(file);
+    }
+
+    protected InputStream getGeneralMappingRuleInputStream()
+            throws FileNotFoundException {
+        String mappingRuleFile = getClass().getClassLoader().
+                getResource("mzQuantML-mapping_1.0.0.xml").getFile();
+        File file = new File(mappingRuleFile);
+        if (!file.exists()) {
+            ClassLoader cl = this.getClass().getClassLoader();
+            return cl.getResourceAsStream("mzQuantML-mapping_1.0.0.xml");
+        }
+        return new FileInputStream(file);
+    }
+
 }
